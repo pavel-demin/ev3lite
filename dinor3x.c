227a458c6c2d50667cfe8a57aa0118853fa96673
Pavel Demin <pavel-demin@outlook.com>
2017-10-19 08:38:22 +0200

    add dinor3x thread

--- dinor3x.c
+++ dinor3x.c
@@ -1,14 +1,14 @@
+#include <pthread.h>
+
 #include "ev3.c"
 
-int main()
+void *dinor3x(void *arg)
 {
-  ev3_init();
+  int i;
 
   /* calibrate legs */
 
-  ev3_output_speed(0x02, 10);
-  ev3_output_speed(0x04, 20);
-  ev3_output_start(0x06);
+  ev3_output_time_sync(0x06, 20, 50, 0, 0);
 
   while(1)
   {
@@ -46,8 +46,6 @@ int main()
   ev3_output_step_speed(0x04, -50, 0, 72, 0,  1);
   ev3_output_wait(0x04);
 
-  ev3_output_clear(0x06);
-
   /* close jaws */
 
   ev3_output_time_speed(0x01, 20, 0, 500, 0,  0);
@@ -55,16 +53,58 @@ int main()
 
   /* main loop */
 
-  ev3_output_time_sync(0x06, -40, 0, 0, 0);
+  while(1)
+  {
+    ev3_output_time_sync(0x06, -40, 0, 0, 0);
+
+    while(1)
+    {
+      if(ev3_uart->Raw[3][ev3_uart->Actual[3]][0] < 25) break;
+      usleep(1000);
+    }
+
+    ev3_output_stop(0x06, 0);
+
+    ev3_output_step_sync(0x06, 75, 0, 1800, 1);
+
+    ev3_output_time_speed(0x01, 40, 0, 100, 0,  1);
+
+    for(i = 0; i < 12; ++i)
+    {
+      ev3_output_time_speed(0x01, -40, 0, 100, 0,  1);
+      ev3_output_wait(0x01);
+      ev3_output_time_speed(0x01, 40, 0, 100, 0,  1);
+      ev3_output_wait(0x01);
+    }
+
+    ev3_output_time_speed(0x01, 20, 0, 500, 0,  0);
+
+    ev3_output_wait(0x06);
+  }
+}
+
+int main()
+{
+  pthread_t thread;
+
+  ev3_init();
+
+  if(pthread_create(&thread, NULL, dinor3x, NULL) < 0)
+  {
+    perror("pthread_create");
+    return EXIT_FAILURE;
+  }
 
   while(1)
   {
-    if(ev3_uart->Raw[3][ev3_uart->Actual[3]][0] < 25) break;
     if(ev3_ui->Pressed[5]) break;
     usleep(1000);
   }
 
-  ev3_output_stop(0x06, 0);
+  pthread_cancel(thread);
+  pthread_join(thread, NULL);
+
+  ev3_output_stop(0x0F, 0);
 
   return EXIT_SUCCESS;
 }
