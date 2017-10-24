bad075ee6e2dd4ae6d1d8bd0bc6444781356768e
Pavel Demin <pavel-demin@outlook.com>
2017-10-25 00:57:07 +0200

    move result verification to send

--- run.py
+++ run.py
@@ -3,76 +3,65 @@ import sys
 import struct
 import serial
 
-def send(port, data):
-  port.write(struct.pack('<H', len(data) + 2) + struct.pack('<H', 0x1234))
+def send(port, code, data):
+  buffer = bytearray()
+  buffer.extend(struct.pack('<H', len(data) + 4))
+  buffer.append(0x34)
+  buffer.append(0x12)
+  buffer.append(0x01)
+  buffer.append(code)
+
+  port.write(buffer)
   port.write(data)
 
-  size = struct.unpack('<H', port.read(2))[0]
-  result = port.read(size)
+  length = struct.unpack('<H', port.read(2))[0]
+  result = port.read(length)
 
-  if(struct.unpack('<H', result[0:2])[0] != 0x1234):
+  if(result[0] != 0x34 or result[1] != 0x12 or result[2] == 0x05 or result[3] != code or result[4] == 0x0a):
     sys.exit('send error')
 
-  return result[2:]
+  return result[5]
 
 def upload(port, path, data):
-  size = len(data)
+  length = len(data)
 
   buffer = bytearray()
-
-  buffer.append(0x01)
-  buffer.append(0x92)
-  buffer.extend(struct.pack('<I', size))
+  buffer.extend(struct.pack('<I', length))
   buffer.extend(path.encode('ascii'))
   buffer.append(0x00)
 
-  result = send(port, buffer)
-
-  if(result[0] == 0x05 or result[1] != 0x92 or result[2] == 0x0A):
-    sys.exit('upload error')
-
-  buffer = bytearray()
-
-  buffer.append(0x01)
-  buffer.append(0x93)
-  buffer.append(result[3])
+  result = send(port, 0x92, buffer)
 
   offset = 0
 
-  while(offset < size):
-    del buffer[3:]
+  while(offset < length):
+    buffer = bytearray()
+    buffer.append(result)
 
-    if(offset + 1016 < size):
+    if(offset + 1016 < length):
       buffer.extend(data[offset:offset + 1016])
       offset += 1016
     else:
       buffer.extend(data[offset:])
-      offset = size
+      offset = length
 
-    result = send(port, buffer)
-
-    if(result[0] == 0x05 or result[1] != 0x93 or result[2] == 0x0A):
-      sys.exit('upload error')
+    send(port, 0x93, buffer)
 
 def start(port, path):
   buffer = bytearray()
-
   buffer.extend(struct.pack('<H', len(path) + 25))
   buffer.extend(bytes.fromhex('3412800020c00882010084'))
   buffer.extend(path.encode('ascii'))
   buffer.extend(bytes.fromhex('00c100c10403820100c100c10400'))
-
   port.write(buffer)
 
 def wrap(path):
   buffer = bytearray()
-
   buffer.extend(b'LEGO')
   buffer.extend(struct.pack('<I', len(path) + 40))
   buffer.extend(bytes.fromhex('6d000100000000001c00000000000000080000006080'))
   buffer.extend(path.encode('ascii'))
   buffer.extend(bytes.fromhex('00448582e8034086400a'))
-  
   return buffer
 
 name = sys.argv[1]
