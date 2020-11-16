3d41b47505affcf46d70475076597b5e68bee9af
Pavel Demin <pavel.demin@uclouvain.be>
2020-11-16 17:07:16 +0100

    replace serial with socket

--- run.py
+++ run.py
@@ -1,36 +1,36 @@
-import os
-import sys
-import struct
-import serial
+from os import system
+from sys import argv, exit
+from struct import pack
+from socket import socket, AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM, MSG_WAITALL
 
-def send(port, code, data):
+def send(sock, code, data):
     buffer = bytearray()
-    buffer.extend(struct.pack('<H', len(data) + 4))
+    buffer.extend(pack('<H', len(data) + 4))
     buffer.extend(bytes.fromhex('341201'))
     buffer.append(code)
 
-    port.write(buffer)
-    port.write(data)
+    sock.send(buffer)
+    sock.send(data)
 
-    result = port.read(8)
+    result = sock.recv(8, MSG_WAITALL)
 
     if (result[0] != 0x06 or result[1] != 0x00 or
         result[2] != 0x34 or result[3] != 0x12 or
         result[4] == 0x05 or result[5] != code or
         result[6] == 0x0a):
-        sys.exit('send error')
+        exit('send error')
 
     return result[7]
 
-def upload(port, path, data):
+def upload(sock, path, data):
     length = len(data)
 
     buffer = bytearray()
-    buffer.extend(struct.pack('<I', length))
+    buffer.extend(pack('<I', length))
     buffer.extend(path.encode('ascii'))
     buffer.append(0x00)
 
-    handle = send(port, 0x92, buffer)
+    handle = send(sock, 0x92, buffer)
 
     offset = 0
 
@@ -45,40 +45,44 @@ def upload(port, path, data):
             buffer.extend(data[offset:])
             offset = length
 
-        send(port, 0x93, buffer)
+        send(sock, 0x93, buffer)
 
-def start(port, path):
+def start(sock, path):
     buffer = bytearray()
-    buffer.extend(struct.pack('<H', len(path) + 17))
+    buffer.extend(pack('<H', len(path) + 17))
     buffer.extend(bytes.fromhex('3412800020c0080184'))
     buffer.extend(path.encode('ascii'))
     buffer.extend(bytes.fromhex('0040440301404400'))
-    port.write(buffer)
+    sock.send(buffer)
 
 def wrap(path):
     buffer = bytearray()
     buffer.extend(b'LEGO')
-    buffer.extend(struct.pack('<I', len(path) + 42))
+    buffer.extend(pack('<I', len(path) + 42))
     buffer.extend(bytes.fromhex('6d000100000000001c00000000000000040000006084'))
     buffer.extend(path.encode('ascii'))
     buffer.extend(bytes.fromhex('00408413000000840083040a'))
     return buffer
 
-name = sys.argv[1]
+addr = argv[1]
+name = argv[2]
 
 path = '../prjs/%s/%s' % (name, name)
 
 command = 'arm-none-linux-gnueabi-gcc %s.c -o %s -lm -lpthread' % (name, name)
-if os.system(command):
-    sys.exit('gcc error')
+if system(command):
+    exit('gcc error')
 
 command = 'arm-none-linux-gnueabi-strip %s' % name
-if os.system(command):
-    sys.exit('strip error')
+if system(command):
+    exit('strip error')
 
-port = serial.Serial('/dev/rfcomm0', 115200)
+sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM)
+sock.connect((addr, 1))
 
 file = open(name, 'rb')
-upload(port, path, file.read())
-upload(port, path + '.rbf', wrap(path))
-start(port, path + '.rbf')
+upload(sock, path, file.read())
+upload(sock, path + '.rbf', wrap(path))
+start(sock, path + '.rbf')
+
+sock.close()
